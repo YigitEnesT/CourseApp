2c9d2f869d2f59d31a0559d8fc5f8b3a8a268176
YigitEnesTuncay <yigit.enes.tuncay@outlook.com>
2024-07-16 16:36:19 +0300

    Final Commit

--- Models/Candidate.cs
+++ Models/Candidate.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CourseApp.Models
 {
     public class Candidate
     {
+        [Required(ErrorMessage = "E-mail is required.")]
         public String? Email { get; set; } = String.Empty;
+        [Required(ErrorMessage = "First name is required.")]
         public String? FirstName { get; set; } = String.Empty;
+        [Required(ErrorMessage = "Last name is required.")]
         public String? LastName { get; set; } = String.Empty;
-        public String? FulName => $"{FirstName} {LastName?.ToUpper()}";
+        public String? FullName => $"{FirstName} {LastName?.ToUpper()}";
         public int? Age { get; set; }
         public String? SelectedCourse { get; set; } = String.Empty;
         public DateTime ApplyAt { get; set; }
